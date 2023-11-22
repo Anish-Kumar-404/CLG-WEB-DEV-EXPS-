2c9e04805e7f71f4d45cf10591523c7a1811cb38
Anish-Kumar-404 <anishkumr.dev@gmail.com>
2023-11-23 02:12:59 +0530

    added EXP11

---
+++ EXP11/phpfile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Session and Cookie Example</title>
+</head>
+<body>
+    <h1> Session and Cookie Example</h1>
+    <form action="page2.php" method="post">
+        <label for="username">Enter Username:</label>
+        <input type="text" id="username" name="username">
+        <br><br>
+        <label for="email">Enter Email:</label>
+        <input type="email" id="email" name="email">
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
